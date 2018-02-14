7934429c529b6f684bc9e45fc813aa1da71911d1
DragosPad <dragospaduraru89@yahoo.com>
2018-02-14 18:23:48 +0200

    delete all projects

---
+++ Accounts/Accounts/CheckingAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    class CheckingAccount
+    {
+        double m_balance;
+        public void Withdraw(double amount)  
+{  
+    if(m_balance >= amount)  
+    {  
+        m_balance -= amount;  
+    }  
+    else  
+    {
+        throw new ArgumentException(amount, "Withdrawal exceeds balance!");  
+    }  
+}  
+  
+    }
+}
